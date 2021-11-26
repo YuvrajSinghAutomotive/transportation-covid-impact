d8534b125a676109f4b72b5a0c1fca8d8592f695
Yuvraj Singh <yuvraj.controlsdev@gmail.com>
2021-11-26 18:28:31 -0500

    implemented all logistic regressions

--- yuvraj.py
+++ yuvraj.py
@@ -48,21 +48,77 @@ XTrain,XTest,YTrain,YTest,idxTrain,idxTest = splitCV(dataDict['X'],
 XTrainPreCovid,XTestPreCovid,YTrainPreCovid,YTestPreCovid,idxTrainPreCovid,idxTestPreCovid = splitCV(dataDictPreCovid['X'],
                                                                                                      dataDictPreCovid['Y'], 
                                                                                                      returnIdx=True).testTrain(testRatio=0.5)
+XTrainPostCovid,XTestPostCovid,YTrainPostCovid,YTestPostCovid,idxTrainPostCovid,idxTestPostCovid = splitCV(dataDictPostCovid['X'], 
+                                                                                                           dataDictPostCovid['Y'], 
+                                                                                                           returnIdx=True).testTrain(testRatio=0.5)
 
 ## base model: logistic regression
-logisticRegr = LogisticRegression()
+Mdls = {'MdlName': ['Logistic Regression'],
+        'Mdl': [ LogisticRegression(max_iter=500) ],
+        'Predictions': [[]],
+        'Test Accuracy': [[]] }
 
-start = time.time()
-logisticRegr.fit(XTrain, YTrain)    ## Train Logistic Regression
-end = time.time()
-print('Time: Logistic Regression (sklearn): {} seconds'.format(end - start) ); 
+MdlsPreCovid = {'MdlName': ['Logistic Regression: Pre-Covid'],
+                'Mdl':[LogisticRegression(max_iter=500)],
+                'Predictions': [[]],
+                'Test Accuracy': [[]] }
 
-pred = []
-for i in range(YTest.shape[0]):
-    pred.append(logisticRegr.predict(XTest[i].reshape(1,-1)))   ## Predict using Logistic Regression
+MdlsPostCovid = {'MdlName': ['Logistic Regression: Post-Covid'],
+                'Mdl':[LogisticRegression(max_iter=500)],
+                'Predictions': [[]],
+                'Test Accuracy': [[]] }
 
-pred = np.array(pred).reshape(YTest.shape)
-print('Logistic Regression Accuracy: {}'.format( np.mean(pred == YTest) ) )
 
-accuracy = np.mean(pred == YTest)
-plotBetaAccuracy(accuracy,XTest.shape[0])
+def fitTestModel(Mdl,MdlName,XTrain,YTrain,XTest,YTest):
+    start = time.time()
+    Mdl.fit(XTrain, YTrain)
+    end = time.time()
+    print('Time: {}: {} seconds'.format(MdlName,end - start) )
+    pred = []
+    for i in range(XTest.shape[0]):
+        pred.append(Mdl.predict(XTest[i].reshape(1,-1)))
+    pred = np.array(pred).reshape(YTest.shape)
+    accuracy = np.mean(pred == YTest)
+    print('Accuracy: {}'.format(accuracy) )
+    plotBetaAccuracy(accuracy,XTest.shape[0])
+    return Mdl,pred,accuracy
+
+for i in range(len(Mdls['Mdl'])):
+    Mdls['Mdl'][i] , Mdls['Predictions'][i], Mdls['Test Accuracy'][i] = fitTestModel(Mdl=Mdls['Mdl'][i],
+                                                                                     MdlName=Mdls['MdlName'][i],
+                                                                                     XTrain=XTrain,
+                                                                                     YTrain=YTrain,
+                                                                                     XTest=XTest,
+                                                                                     YTest=YTest)
+
+for i in range(len(MdlsPreCovid['Mdl'])):
+    MdlsPreCovid['Mdl'][i] , MdlsPreCovid['Predictions'][i], MdlsPreCovid['Test Accuracy'][i] = fitTestModel(Mdl=MdlsPreCovid['Mdl'][i],
+                                                                                                             MdlName=MdlsPreCovid['MdlName'][i],
+                                                                                                             XTrain=XTrainPreCovid,
+                                                                                                             YTrain=YTrainPreCovid,
+                                                                                                             XTest=XTestPreCovid,
+                                                                                                             YTest=YTestPreCovid)
+
+for i in range(len(MdlsPostCovid['Mdl'])):
+    MdlsPostCovid['Mdl'][i] , MdlsPostCovid['Predictions'][i], MdlsPostCovid['Test Accuracy'][i] = fitTestModel(Mdl=MdlsPostCovid['Mdl'][i],
+                                                                                                                MdlName=MdlsPostCovid['MdlName'][i],
+                                                                                                                XTrain=XTrainPostCovid,
+                                                                                                                YTrain=YTrainPostCovid,
+                                                                                                                XTest=XTestPostCovid,
+                                                                                                                YTest=YTestPostCovid)
+
+# pred = []
+# predPreCovid = []
+# predPostCovid = []
+# for i in range(YTest.shape[0]):
+#     pred.append(logisticRegr.predict(XTest[i].reshape(1,-1)))   ## Predict using Logistic Regression
+# for i in range(YTestPreCovid.shape[0]):
+#     predPreCovid.append(logisticRegrPreCovid.predict(XTestPreCovid[i].reshape(1,-1)))   ## Predict using Logistic Regression
+# for i in range(YTestPostCovid.shape[0]):
+#     predPostCovid.append(logisticRegrPostCovid.predict(XTestPostCovid[i].reshape(1,-1)))   ## Predict using Logistic Regression
+
+# pred = np.array(pred).reshape(YTest.shape)
+# print('Logistic Regression Accuracy: {}'.format( np.mean(pred == YTest) ) )
+
+# accuracy = np.mean(pred == YTest)
+# plotBetaAccuracy(accuracy,XTest.shape[0])
